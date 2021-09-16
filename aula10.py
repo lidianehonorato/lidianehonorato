442839d2863147e4f646b97c17780b72d3002b6f
Lidiane Honorato <lidianehonorato@yahoo.com.br>
2021-09-16 20:24:03 -0300

    atualização

--- aula10.py
+++ aula10.py
@@ -5,6 +5,7 @@ else: print('carro velho')
 print('--fim---')
 
 
+# segunda forma de montar a solução codigo simplificado
 tempo = int(input('quantos anos tem o seu carro?  '))
 print('carro novo' if tempo <=3 else'carro velho')
 print('--fim---')
